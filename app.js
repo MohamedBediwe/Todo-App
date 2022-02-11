a78fef36ec6c9afdf24b97218684ffc892aacd1d
MohamedBediwe <mohamed.bediwe@gmail.com>
2022-02-12 00:34:12 +0200

    The Full App

---
+++ app.js
@@ -0,0 +1,192 @@
+const modePicker = document.querySelector("header .image")
+const inputField = document.querySelector(".input input")
+const container = document.querySelector(".container")
+const listElement = document.querySelector('.tasks ul')
+const addBtn = document.querySelector('.input .add')
+const clearCompletedBtn = document.querySelector('.clear-com')
+let storageArray = []
+let tasksFromLocalStorage = localStorage.getItem('Tasks')
+
+if (tasksFromLocalStorage) {
+
+    storageArray = JSON.parse(tasksFromLocalStorage)
+    for (let i = 0; i < storageArray.length; i++) {
+        createLi(storageArray[i].text, storageArray[i].done, storageArray[i].id)
+    }
+    countLeft()
+}
+
+// Theme Changer function
+
+if (localStorage.getItem("mode") === "dark") {
+
+    document.body.classList.add("dark")
+    container.classList.add("dark")
+}
+
+modePicker.addEventListener("click", function () {
+
+    document.body.classList.toggle("dark");
+    container.classList.toggle("dark");
+
+    if (container.classList.contains("dark")) {
+
+        localStorage.setItem("mode", "dark")
+    }
+    else {
+        localStorage.setItem("mode", "light")
+    }
+})
+// Function to make the add button work
+
+addBtn.addEventListener('click', function(e) {
+
+    if (!inputField.value) return;
+    const taskObj = {
+        id: Date.now(),
+        done: false,
+        text: inputField.value
+    }
+
+    storageArray.push(taskObj)
+    localStorage.setItem('Tasks', JSON.stringify(storageArray))
+    createLi(taskObj.text, taskObj.done, taskObj.id)
+    inputField.value = ""
+    countLeft()
+})
+// Function of making the li elements and appending the elements to page
+
+function createLi(text, done, id) {
+    const li = document.createElement('li');
+
+    // done btn
+    const circleSpan = document.createElement('span')
+    circleSpan.className = "circle"
+    li.appendChild(circleSpan)
+
+    // Complete Task Function
+
+    circleSpan.addEventListener('click', function (ele) {
+
+        li.classList.toggle('done')
+        if (li.classList.contains('done')) {
+
+            storageArray.forEach(function (item) {
+                if (item.id == ele.currentTarget.parentElement.getAttribute('data-id')) {
+                    item.done = true
+                }
+            })
+        } 
+        else {
+
+            storageArray.forEach(function (item) {
+                if (item.id == ele.currentTarget.parentElement.getAttribute('data-id')) {
+                    item.done = false
+                }
+            })
+        }
+        localStorage.setItem('Tasks', JSON.stringify(storageArray))
+        countLeft()
+    })
+    li.appendChild(document.createTextNode(text))
+    // delete btn
+    const delSpan = document.createElement('span')
+    delSpan.className = "delete"
+    li.appendChild(delSpan)
+
+    // Delete Function
+    delSpan.addEventListener('click', function (ele) {
+
+        storageArray = storageArray.filter(function (item) {
+
+            return item.id != ele.currentTarget.parentElement.getAttribute('data-id')
+        })
+        localStorage.setItem('Tasks', JSON.stringify(storageArray))
+        ele.currentTarget.parentElement.remove();
+        countLeft();
+    })
+
+    // Adding done class
+
+    if (done) {
+        li.className = "done"
+    }
+
+    // Adding the id
+
+    li.setAttribute("data-id", id)
+    listElement.appendChild(li)
+}
+// how many Tasks left
+
+function countLeft() {
+
+    let unDone = document.querySelectorAll("ul li").length - document.querySelectorAll("ul li.done").length;
+    const number = document.querySelector('.number');
+    number.textContent = unDone
+}
+
+// Shuffle Functions
+
+const all = document.querySelector('.all')
+const comp = document.querySelector('.completed')
+const active = document.querySelector(".active")
+
+all.addEventListener('click', function () {
+    if (all.classList.contains('shown')) return;
+        active.classList.remove("shown")
+        comp.classList.remove('shown')
+        all.classList.add('shown')
+        listElement.innerHTML = ""
+        for (let li of storageArray) {
+            createLi(li.text, li.done, li.id)
+        }
+})
+
+active.addEventListener('click', function () {
+
+    if (active.classList.contains('shown')) return;
+        all.classList.remove("shown")
+        comp.classList.remove('shown')
+        active.classList.add('shown')
+        listElement.innerHTML = ""
+        storageArray.filter(function (e) {
+            return e.done === false
+        }).forEach((e) => {
+            createLi(e.text, e.done, e.id)
+        })
+})
+
+comp.addEventListener('click', function () {
+
+    if (comp.classList.contains('shown')) return 
+        all.classList.remove('shown')
+        active.classList.remove('shown')
+        comp.classList.add('shown')
+        listElement.innerHTML = ""
+
+        storageArray.filter(function (e) {
+            return e.done === true
+        }).forEach((e) => {
+            createLi(e.text, e.done, e.id)
+        })
+})
+
+// Function to clear completed Tasks
+
+clearCompletedBtn.addEventListener('click', function () {
+
+    listElement.innerHTML = ""
+    storageArray = storageArray.filter(function (ele) {
+        return ele.done === false
+    })
+    storageArray.forEach((e) => {
+        createLi(e.text, e.done, e.id)
+    })
+    
+    localStorage.setItem('Tasks', JSON.stringify(storageArray))
+    if (comp.classList.contains('shown')) {
+        comp.classList.remove('shown')
+        all.classList.add('shown');
+    }
+})
